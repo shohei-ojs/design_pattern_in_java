a8a399b30d59479aa5829983f8942f5dbb3fe0eb
shohei <shohei.ojs@gmail.com>
2017-09-06 14:32:55 +0000

    ディレクトリ構成変更

--- factory_method/Main.java
+++ factory_method/Main.java
@@ -3,8 +3,8 @@ import idcard.*;
 
 public class Main {
   public static void main(String[] args) {
-    Factory = new IDCardFactory();
-    Product card1 = factory.create("SHOHEI");
+    Factory factory = new IDCardFactory();
+    Product card1   = factory.create("SHOHEI");
     card1.use();
   }
 }
