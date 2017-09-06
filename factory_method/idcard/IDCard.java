e1a2d89dc0bc189b84afbc37c8f5d1d2ee12c77a
shohei <shohei.ojs@gmail.com>
2017-09-06 14:34:16 +0000

    factory_method

---
+++ factory_method/idcard/IDCard.java
@@ -0,0 +1,18 @@
+package idcard;
+import framework.*;
+
+public class IDCard extends Product {
+  private String owner;
+  IDCard(String owner) {
+    System.out.println(owner + "のカードを作ります。");
+    this.owner = owner;
+  }
+  
+  public void use() {
+    System.out.println(owner + "のカードを作ります。");
+  }
+  
+  public String getOwner() {
+    return owner;
+  }
+}
