887d2f2288489b7664a2100e664d20c6509d016f
shohei <shohei.ojs@gmail.com>
2017-09-15 00:34:51 +0000

    abstract_factory

--- abstract_factory/Main.java
+++ abstract_factory/Main.java
@@ -8,7 +8,31 @@ public class Main {
     }
     Factory factory = Factory.getFactory(args[0]);
     
-    Link asahi    = factory.createLink("朝日新聞", "htto://www.asahi.com/")
-    Link yomiuri  = factory.createLink("読売新聞", "htto://www.yomiuri.co.jp/")
+    Link asahi    = factory.createLink("朝日新聞", "htto://www.asahi.com/");
+    Link yomiuri  = factory.createLink("読売新聞", "htto://www.yomiuri.co.jp/");
+    
+    Link us_yahoo = factory.createLink("Yahoo!",   "http://www.yahoo.com/");
+    Link jp_yahoo = factory.createLink("Yahoo!Japan",   "http://www.yahoo.co.jp/");
+    Link excite   = factory.createLink("Excite",   "http://www.excite.com/");
+    Link google   = factory.createLink("Google",   "http://www.google.com/");
+    
+    Tray traynews = factory.createTray("Yahoo!");
+    traynews.add(asahi);
+    traynews.add(yomiuri);
+    
+    Tray trayyahoo = factory.createTray("Yahoo");
+    trayyahoo.add(us_yahoo);
+    trayyahoo.add(jp_yahoo);
+    
+    Tray traysearch = factory.createTray("サーチエンジン");
+    traysearch.add(trayyahoo);
+    traysearch.add(excite);
+    traysearch.add(google);
+    
+    Page page = factory.createPage("LinkPage", "結城　浩");
+    page.add(traynews);
+    page.add(traysearch);
+    page.output();
+    
   }
 }
